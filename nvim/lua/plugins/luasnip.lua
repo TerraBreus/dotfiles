a2bfa7e882bfc493eaa007110184f82c8f92f957
TerraBreus <studentbreus@gmail.com>
2026-02-21 13:46:38 +0100

    adding luasnippets

---
+++ nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,25 @@
+return
+{
+	{
+	"L3MON4D3/LuaSnip",
+	-- follow latest release.
+	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	config = function()
+			local ls = require("luasnip")
+
+			vim.keymap.set({"i", "s"}, "<Tab>", function()
+					if ls.expand_or_jumpable() then
+						ls.expand_or_jump()
+					else
+						vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+					end
+				end, {silent = true})
+
+			vim.keymap.set({"i", "s"}, "<S-Tab>", function()
+					ls.jump(-1)
+				end, {silent = true})
+
+			require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/snippets/"})
+		end,
+	},
+}
---
+++ nvim/snippets/cpp.lua
@@ -0,0 +1,27 @@
+-- AI GENERATED CODE CAUSE I AM NOT YET CONFIDENT TO WRITE MY OWN NVIM CONFIG
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+
+-- Helper to repeat the class name
+local function copy(args)
+	return args[1][1]
+end
+
+return {
+	s("canon", {
+		t("class "), i(1, "ClassName"), t({ " {", "\tpublic:", "\t\t" }),
+		-- Default Constructor
+		f(copy, { 1 }), t({ "(void);", "\t\t" }),
+		-- Copy Constructor
+		f(copy, { 1 }), t("(const "), f(copy, { 1 }), t({ "& other);", "\t\t" }),
+		-- Assignment Operator
+		f(copy, { 1 }), t("& operator=(const "), f(copy, { 1 }), t({ "& other);", "\t\t" }),
+		-- Destructor
+		t("~"), f(copy, { 1 }), t({ "(void);", "", "\tprivate:", "\t\t" }),
+		i(0),
+		t({ "", "};" }),
+	}),
+}
